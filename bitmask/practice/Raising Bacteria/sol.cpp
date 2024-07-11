5d725d09fbff7d02a26cf227bf059f2d1964ab64
AbdelrahmanAyman0011 <abdelrahmanayman0011@gmail.com>
2024-07-11 03:02:39 +0300

    bitmask

---
+++ bitmask/practice/Raising Bacteria/sol.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(v) v.begin(), v.end()
+#define Bedo cin.tie(0); cout.tie(0); ios::sync_with_stdio(false);
+#define yes cout <<"YES" << endl;
+#define no cout <<"NO" << endl;
+const long double PI = acos(-1.0);
+
+void solve() {
+    ll x; cin >> x;
+    // ll res = 0;
+    // while(x){
+    //     if(x & 1)
+    //         res++;
+    //     x/=2;
+    // }
+    // cout << res;
+    cout << __builtin_popcount(x);
+}
+
+int main() {
+    Bedo
+    int tt = 1;
+    //cin >> tt;
+    while(tt--) {
+        solve();
+    }
+    return 0;
+}
